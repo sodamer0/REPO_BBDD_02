82fa781b9a284ba0a7a0661198fc2776282c2a68
sodamer0H <pr0gram3r0@gmail.com>
2026-02-09 00:07:34 +0100

    domingo

---
+++ EJERCICIOS BÁSICOS/Ejercicios de Consultas-20260120/1. Consultas básicas/CONSULTAS_BASICAS_02.sql
@@ -0,0 +1,17 @@
+/*5.	Base de datos DepartamentoOficinas (5 tablas).
+ 
+Muestra toda la información de la tabla empleados.
+
+Muestra toda la información de la tabla departamento.
+
+Muestra el código y nombre de las categorías del personal.
+
+*/
+
+-- Muestra toda la información de la tabla empleados.
+
+	SELECT *
+	FROM empleado e 
+	ORDER BY departamento ;
+	
+-- Muestra toda la información de la tabla departamento.
